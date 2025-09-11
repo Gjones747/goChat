cd7b779e595ae921b9696303358cd1a48c6d069a
cJones <crman210@gmail.com>
2025-09-11 21:51:13 +0000

    editing for deployment

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,50 +1,56 @@
 package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
 
-	"github.com/Gjones747/goChat/server/models"
-	"github.com/Gjones747/goChat/server/routes"
+    "github.com/Gjones747/goChat/server/models"
+    "github.com/Gjones747/goChat/server/routes"
 )
 
 func main() {
-	mux := http.NewServeMux()
-	roomHub := models.MakeRoomHub()
+    mux := http.NewServeMux()
+    roomHub := models.MakeRoomHub()
 
-	createRoomHandler := func(w http.ResponseWriter, r *http.Request) {
+    createRoomHandler := func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != "GET" {
+            http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+            return
+        }
+        log.Println("Request Sent to /makeRoom")
+        roomCode := r.URL.Query().Get("room_code")
+        if roomCode == "" {
+            log.Println("bro you gotta send a room code when creating a room")
+            return
+        }
+        routes.CreateRoom(w, r, roomHub, roomCode)
+    }
 
-		log.Println("Request Sent to /makeRoom")
-		roomCode := r.URL.Query().Get("room_code")
-		if roomCode == "" {
-			log.Println("bro you gotta send a room code when creating a room")
-			return
-		}
+    joinRoomHandler := func(w http.ResponseWriter, r *http.Request) {
+        if r.Method != "GET" {
+            http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+            return
+        }
+        roomCode := r.URL.Query().Get("room_code")
+        if roomCode == "" {
+            log.Println("bro you gotta send a room code when creating a room")
+            return
+        }
+        if _, ok := roomHub.Rooms[roomCode]; !ok {
+            routes.CreateRoom(w, r, roomHub, roomCode)
+            log.Println("Room doesnt exist - making a new one anyway")
+            return
+        }
+        routes.JoinRoom(w, r, roomHub, roomCode)
+    }
 
-		routes.CreateRoom(w, r, roomHub, roomCode)
-	}
+    mux.HandleFunc("/makeRoom", createRoomHandler)
+    mux.HandleFunc("/joinRoom", joinRoomHandler)
 
-	joinRoomHandler := func(w http.ResponseWriter, r *http.Request) {
-		roomCode := r.URL.Query().Get("room_code")
-		if roomCode == "" {
-			log.Println("bro you gotta send a room code when creating a room")
-			return
-		}
-
-		// checks to make sure the room exists if it doesnt it will call the createroom func
-
-		if _, ok := roomHub.Rooms[roomCode]; !ok {
-			routes.CreateRoom(w, r, roomHub, roomCode)
-			log.Println("Room doesnt exist - making a new one anyway")
-			return
-		}
-		routes.JoinRoom(w, r, roomHub, roomCode)
-	}
-
-	// Endpoint is essentially ..../makeRoom?room_code={whatever}&user_name={username}
-	// spawns a new room and automatically adds the user to the room
-	mux.Handle("GET /makeRoom", http.HandlerFunc(createRoomHandler))
-	mux.Handle("GET /joinRoom", http.HandlerFunc(joinRoomHandler))
-
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", mux))
+    mux.HandleFunc("/chat", func(w http.ResponseWriter, r *http.Request) {
+        http.ServeFile(w, r, "html/index.html")
+    })
+	mux.Handle("/chat/", http.StripPrefix("/chat/", http.FileServer(http.Dir("html"))))
+    log.Println("Server starting on port 8081")
+    log.Fatal(http.ListenAndServe("0.0.0.0:8081", mux))
 }
