0dce18ab697d87303449f06c9b69d6aab2866ddc
Gjones747 <gsman707@gmail.com>
2025-09-13 16:54:47 -0700

    dockerfile for deploy?

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/Gjones747/goChat/server/models"
 	"github.com/Gjones747/goChat/server/routes"
@@ -11,6 +12,12 @@ import (
 func main() {
 	mux := http.NewServeMux()
 	roomHub := models.MakeRoomHub()
+
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8080"
+	}
 
 	createRoomHandler := func(w http.ResponseWriter, r *http.Request) {
 
@@ -46,5 +53,5 @@ func main() {
 	mux.Handle("GET /makeRoom", http.HandlerFunc(createRoomHandler))
 	mux.Handle("GET /joinRoom", http.HandlerFunc(joinRoomHandler))
 
-	log.Fatal(http.ListenAndServe("0.0.0.0:8080", mux))
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+port, mux))
 }
