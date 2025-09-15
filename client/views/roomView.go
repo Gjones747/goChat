fce495a3bf4a9fa00460f81418ecf2e1824c422d
Gjones747 <gsman707@gmail.com>
2025-09-15 01:11:03 -0700

    working on deploy

--- client/views/roomView.go
+++ client/views/roomView.go
@@ -272,7 +272,7 @@ func (m roomView) renderFooter() string {
 // TODO dot env
 func (m roomView) initializeConnection() (net.Conn, error) {
 
-	url := url.URL{Scheme: "ws", Host: "localhost:8080", Path: "/joinRoom"}
+	url := url.URL{Scheme: "ws", Host: "kaolun.site:8081", Path: "/joinRoom"}
 	q := url.Query()
 	q.Add("room_code", m.roomCode)
 	q.Add("user_name", m.userName)
