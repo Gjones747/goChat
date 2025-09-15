a494e790ab72d3d4af8fc1a58c07d0ce448e3f36
Gjones747 <gsman707@gmail.com>
2025-09-15 15:09:09 -0700

    added the session stat to the user list

--- api/user.go
+++ api/user.go
@@ -3,5 +3,5 @@ package api
 //this will be the logic for updating the listed users in a room
 
 type UserList struct {
-	Users []string // each list includes a [username, sessionID]
+	Users [][]string // each list includes a [username, sessionID]
 }
